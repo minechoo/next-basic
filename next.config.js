537252a17386adf28a10b2fd15cab132ec1119e3
minechoo <choominhae@gmail.com>
2023-08-03 10:14:14 +0900

    first

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
+const path = require('path');
 const nextConfig = {
-  reactStrictMode: true,
-}
+	reactStrictMode: false,
+	sassOptions: {
+		includePaths: [path.join(__dirname, 'styles')],
+		prependData: `@import "styles/_variables.scss";`,
+	},
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
-import '@/styles/globals.css'
+import '@/styles/globals.scss';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+	return <Component {...pageProps} />;
 }
+// root component : 페이지관리 설정 파일
