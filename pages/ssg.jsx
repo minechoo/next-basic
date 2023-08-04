11d8c48051398be89e072fe0bcd4105fb79162e9
minechoo <choominhae@gmail.com>
2023-08-04 13:10:19 +0900

    ssg로 department

--- pages/ssg.jsx
+++ pages/ssg.jsx
@@ -1,10 +1,19 @@
 import SubLayout from '@/components/SubLayout';
+import data from '@/public/members.json';
 
 function Ssg(props) {
 	return (
 		<SubLayout name={'SSG'}>
 			<p>SSG방식 테스트 페이지입니다</p>
 			<h1>{props.now}</h1>
+			{props.members.map((member, idx) => {
+				return (
+					<article key={idx}>
+						<h2>{member.name}</h2>
+						<p>{member.position}</p>
+					</article>
+				);
+			})}
 		</SubLayout>
 	);
 }
@@ -13,7 +22,7 @@ export async function getStaticProps() {
 	//해당 프로젝트를 빌드시에 한번만 호출
 	console.log('ssg');
 	return {
-		props: { now: performance.now() },
+		props: { now: performance.now(), members: data.members },
 	};
 }
 
